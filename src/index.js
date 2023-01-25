ecfe5761f78731e417090ab605ecccb175a3f3d0
June <applause1319@naver.com>
2023-01-25 17:33:59 +0900

    feat: js 로그인 끝 - 쿠키 세팅 - 조건부 렌더링 - 디바운스 - 기타 등등

--- src/index.js
+++ src/index.js
@@ -1,67 +1,121 @@
 import './style.css'
 import './reset.css'
+import entErrImg from './asset/icon/no_network_icon.png'
 import { getCookie, setCookie } from "./module/cookie";
 
 console.log('index.js')
 
 /**
- * Check if there is a configuration file
- * @param infoMessageValue {Object} - Object with information login the configuration file
- * @returns {Promise<void>}
+ * 브라우저 실행 시 Config File Check
  */
-// const infoMessage = document.querySelector('.info-message');
-const checkConfigFile = async () => {
-  const infoMessage = document.querySelector('.info-message');
-
-  const infoMessageValue = {
-    init: `<div class="info-value"><p>Validate the data in the database</p></div>`,
-    checkRules: `<div class="info-value"><p>The configuration file does not exist</p><p>Try to download rules file</p></div>`,
-    checkEventConfig: `<div class="info-value"><p>The configuration file does not exist</p><p>Try to download Event Config file</p></div>`,
-    check: `<div class="info-value"><p>Check if there is a configuration file</p></div>`,
-  }
-
-  for (let info in infoMessageValue) {
-    if (info === 'init') {
-      console.log('init')
-      infoMessage.insertAdjacentHTML('beforeend', infoMessageValue[info]);
-      continue;
-    }
-    const cookieCheck = getCookie('fileToken');
-
-    if (cookieCheck) {
-      location.href = 'login/login.html';
-      return;
-    }
-
-    await changeInfoMessage(info, infoMessage, infoMessageValue);
-  }
+window.onload = () => {
+    console.log('errorFlag', errorFlag)
+    checkConfigFile();
 }
 
 /**
- * Change the information message
+ * Error Flag
+ * @type {boolean}
+ */
+let errorFlag = false;
+
+const errorBtn = document.querySelector('.error-btn')
+errorBtn.addEventListener('click', (event) => {
+    getError()
+})
+
+
+/**
+ * config file을 체크하고 file token이 있으면 로그인 페이지로 이동
+ * @returns {Promise<void>}
+ */
+const checkConfigFile = async () => {
+    const errorWrapper = document.querySelector('.error-wrapper')
+    const indexWrapper = document.querySelector('.index-wrapper')
+    const infoMessage = document.querySelector('.info-message');
+    const infoValue = document.querySelector('.info-value');
+    const infoMessageValue = {
+        init: `<div class="info-value"><p>Validate the data in the database</p></div>`,
+        checkRules: `<div class="info-value"><p>The configuration file does not exist</p><p>Try to download rules file</p></div>`,
+        checkEventConfig: `<div class="info-value"><p>The configuration file does not exist</p><p>Try to download Event Config file</p></div>`,
+        check: `<div class="info-value"><p>Check if there is a configuration file</p></div>`,
+    }
+    
+    if (errorFlag) {
+        errorFlag = false;
+        errorWrapper.style.display = 'none'
+        indexWrapper.style.display = 'flex'
+    }
+    
+    for (let info in infoMessageValue) {
+        if (infoValue) {
+            infoValue.remove()
+        }
+        
+        if (info === 'init') {
+            infoMessage.insertAdjacentHTML('beforeend', infoMessageValue[info]);
+            continue;
+        }
+        const cookieCheck = getCookie('fileToken');
+        
+        if (cookieCheck) {
+            location.href = 'login/login.html';
+            return;
+        }
+        
+        await changeInfoMessage(info, infoMessage, infoMessageValue);
+    }
+}
+
+/**
+ * information message 변경 error flag가 true면 return
  * @param info {string} - Information message
  * @param infoMessage {HTMLElement} - Information message element
  * @param infoMessageValue {Object} - Object with information login the configuration file
  * @returns {Promise<void>}
  */
 const changeInfoMessage = async (info, infoMessage, infoMessageValue) => {
-  await new Promise(resolve => {
-    setTimeout(() => {
-      const infoValue = document.querySelector('.info-value');
-      if (infoValue) {
-        infoValue.remove()
-      }
-      infoMessage.insertAdjacentHTML('beforeend', infoMessageValue[info]);
-      resolve();
-    }, 1500);
-
-    if (info === 'check') {
-      setCookie('fileToken', true);
-      setTimeout(() => {
-        location.href = 'login/login.html';
-      }, 2000);
+    if (errorFlag) {
+        return
     }
-  })
+    await new Promise(resolve => {
+        setTimeout(() => {
+            const infoValue = document.querySelector('.info-value');
+            if (infoValue) {
+                infoValue.remove()
+            }
+            infoMessage.insertAdjacentHTML('beforeend', infoMessageValue[info]);
+            resolve();
+        }, 1500);
+    }).then(() => {
+        if (info === 'check') {
+            setCookie('fileToken', true);
+            setTimeout(() => {
+                location.href = 'login/login.html';
+            }, 2000);
+        }
+    }).catch(err => getError())
 }
 
-checkConfigFile();
+/**
+ * 로딩바 숨기고 에러메세지 출력
+ * 버튼 클릭 시 checkConfigFile() 실행
+ */
+const getError = () => {
+    errorFlag = true;
+    const indexWrapper = document.querySelector('.index-wrapper')
+    const errorWrapper = document.querySelector('.error-wrapper')
+    const errorImg = document.querySelector('.error-img')
+    const retryBtn = document.querySelector('.retry-btn')
+    errorImg.src = entErrImg
+    indexWrapper.style.display = 'none'
+    errorWrapper.style.display = 'flex'
+    
+    retryBtn.addEventListener('click', (event) => {
+        event.preventDefault()
+        console.log('retryBtn')
+        checkConfigFile();
+    })
+}
+
+
--- src/login/login.js
+++ src/login/login.js
@@ -3,7 +3,6 @@ import '../reset.css'
 import background from '../asset/callSignBg.png'
 import { deleteCookie, getCookie, setCookie } from "../module/cookie";
 import * as setLoginAtt from './setLoginAtt'
-import * as modal from '../module/modal/modal.js'
 
 console.log('Hello ImoLogin')
 
@@ -14,77 +13,106 @@ loginBackground.style.backgroundImage = `url(${ background })`
 
 const delFileCookieBtn = document.querySelector('.del-cookie-btn__file')
 delFileCookieBtn.addEventListener('click', () => {
-  deleteCookie('fileToken')
+    deleteCookie('fileToken')
 })
 
 const delCodeCookieBtn = document.querySelector('.del-cookie-btn__code')
 delCodeCookieBtn.addEventListener('click', () => {
-  deleteCookie('codeToken')
+    deleteCookie('codeToken')
 })
 
+/**
+ * 브라우저 실행 시 fileToken이 없으면 index.html로 이동
+ * 있으면 input setting
+ */
 window.onload = () => {
-  if (!getCookie('fileToken')) {
-    location.href = '../index.html'
-  }
-  setImoInputEvent()
-  modal.modalOn()
+    if (!getCookie('fileToken')) {
+        location.href = '../index.html'
+    }
+    setImoInputEvent()
 }
 
+/**
+ * imo input에 event 세팅
+ */
 const setImoInputEvent = () => {
-  const imoInput = document.querySelectorAll('.imo-input')
-  const loginBtn = document.querySelector('.imo-login-btn')
-
-  if (!imoInput[0].value || !imoInput[1].value) {
-    loginBtn.disabled = true
-  }
-
-  imoInput.forEach((input) => {
-    input.addEventListener('input', (event) => {
-      const value = event.target.value
-      if (!value.length) {
+    const imoInput = document.querySelectorAll('.imo-input')
+    const loginBtn = document.querySelector('.imo-login-btn')
+    
+    // 두 값이 falsy값이면 btn disabled
+    if (!imoInput[0].value || !imoInput[1].value) {
         loginBtn.disabled = true
-      }
-
-      if (imoInput[0].value !== imoInput[1].value) {
-        loginBtn.disabled = true
-      }
-
-      if (imoInput[0].value === imoInput[1].value) {
-        loginBtn.disabled = false
-      }
+    }
+    
+    // 유사배열 array form
+    // console.log('imoInput', imoInput)
+    // imoInput.map((input) => {
+    //     console.log('input', input)
+    // })
+    
+    // 각 input에 이벤트 세팅
+    imoInput.forEach((input) => {
+        input.addEventListener('input', (event) => {
+            const value = event.target.value
+            // 디바운스
+            if (debouncingInput) clearTimeout(debouncingInput);
+            let debouncingInput = setTimeout(() => {
+                // input 길이가 0이면 btn disabled
+                if (!value.length) {
+                    loginBtn.disabled = true
+                }
+                
+                // 두 값이 다르면 btn disabled
+                if (imoInput[0].value !== imoInput[1].value) {
+                    loginBtn.disabled = true
+                }
+                
+                // 두 값이 같으면 btn enabled
+                // if (imoInput[0].value.length === imoInput[1].value.length) {
+                if (imoInput[0].value === imoInput[1].value) {
+                    loginBtn.disabled = false
+                }
+            }, 250);
+        })
     })
-  })
-
-  loginBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-    if (imoInput[0].value !== imoInput[1].value) {
-      alert('비밀번호가 일치하지 않습니다.')
-      initInput(imoInput)
-      imoInput[0].focus()
-      return
-    }
-
-    if (imoInput[0].value === imoInput[1].value) {
-      const codeToken = getCookie('codeToken')
-      codeToken ? goLoading() : codeTokenFalse()
-      initInput(imoInput)
-    }
-  })
+    
+    // 로그인 버튼 클릭 시
+    loginBtn.addEventListener('click', (event) => {
+        // submit 기본 동작 막기
+        event.preventDefault()
+        // 두값이 다르면 알림
+        if (imoInput[0].value !== imoInput[1].value) {
+            alert('비밀번호가 일치하지 않습니다.')
+            initInput(imoInput)
+            imoInput[0].focus()
+            return
+        }
+        
+        // 두값이 같고 codeToken이 있으면 로딩화면으로 없으면 code Input
+        if (imoInput[0].value === imoInput[1].value) {
+            const codeToken = getCookie('codeToken')
+            codeToken ? goLoading() : codeTokenFalse()
+            initInput(imoInput)
+        }
+    })
 }
 
+/**
+ * 로딩 화면으로 이동
+ */
 const goLoading = () => {
-  const InputWrapper = document.querySelector('.input-wrapper')
-
-  removeChild(InputWrapper)
-
-  InputWrapper.className = `loading-wrapper`
-  let loadingPercentNum = 0
-  let loadingPercent = `${ loadingPercentNum }%`
-
-  const { loadingValue, loadingBar } = setLoginAtt.setLoadingBar(InputWrapper, loadingPercentNum, loadingPercent)
-
-  startLoadingInterval(loadingPercentNum, loadingPercent, loadingValue, loadingBar)
-  // loadingPercentNum = setInterval(startLoadingInterval, 150, loadingPercentNum, loadingPercent, loadingValue, loadingBar)
+    const InputWrapper = document.querySelector('.input-wrapper')
+    
+    removeChild(InputWrapper)
+    
+    InputWrapper.className = `loading-wrapper`
+    let loadingPercentNum = 0
+    let loadingPercent = `${ loadingPercentNum }%`
+    
+    const { loadingValue, loadingBar } = setLoginAtt.setLoadingBar(InputWrapper, loadingPercentNum, loadingPercent)
+    
+    startLoadingInterval(loadingPercentNum, loadingPercent, loadingValue, loadingBar)
+    // loadingPercentNum = setInterval(startLoadingInterval, 150, loadingPercentNum, loadingPercent, loadingValue, loadingBar)
 }
 
 // const startLoadingInterval = (loadingPercentNum, loadingPercent, loadingValue, loadingBar) => {
@@ -100,82 +128,114 @@ const goLoading = () => {
 //   }
 // }
 
-const startLoadingInterval = (loadingPercentNum, loadingPercent, loadingValue, loadingBar) => {
-  const loadingInterval = setInterval(() => {
-    loadingPercentNum += 1
-    loadingPercent = `${ loadingPercentNum }%`
-    loadingValue.textContent = loadingPercent
-    loadingBar.setAttribute('value', `${ loadingPercentNum }`)
-    if (loadingPercentNum === 100) {
-      clearInterval(loadingInterval)
-      location.href = '../main/main.html'
-    }
-  }, 50)
-}
-
+/**
+ * dom 자식 삭제 공통
+ * @param parent
+ */
 const removeChild = (parent) => {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild)
-  }
-}
-
-const initInput = (inputs) => {
-  inputs.forEach((input) => {
-    input.value = ''
-  })
-}
-
-const codeTokenFalse = () => {
-  const inputWrapper = document.querySelector('.input-wrapper')
-  const InputForm = document.querySelector('.login-input-wrapper')
-  const inputTitle = document.querySelector('.input-title')
-  const inputSubTitle = document.querySelector('.input-title__sub')
-
-  inputTitle.textContent = 'Please enter the code.'
-  inputSubTitle.textContent = 'Please contact the shipping company for confirmation code to register your new machine and enter the code you received.'
-
-  // while (InputForm.firstChild) {
-  //   InputForm.removeChild(InputForm.firstChild)
-  // }
-  removeChild(InputForm)
-  changeToInputCode(InputForm)
-  setLoginAtt.setContact(inputWrapper)
-}
-
-const changeToInputCode = (InputForm) => {
-  for (let i = 0; i < 4; i++) {
-    const codeInput = document.createElement('input')
-    codeInput.setAttribute('class', `code-input code-input-${ i }`)
-    codeInput.setAttribute('type', 'text')
-    codeInput.setAttribute('maxlength', '1')
-    InputForm.appendChild(codeInput)
-    codeInput.addEventListener('keyup', onInputCode)
-  }
-}
-
-const onInputCode = (event) => {
-  const codeInput = document.querySelectorAll('.code-input')
-  let verifyCode = ''
-
-  if (event.target.value.length === 1 && event.target.nextElementSibling) {
-    event.target.nextElementSibling.focus()
-  }
-
-  if (event.keyCode === 8 && event.target.previousElementSibling) {
-    event.target.previousElementSibling.focus()
-  }
-
-  verifyCode = codeInput[0].value + codeInput[1].value + codeInput[2].value + codeInput[3].value
-
-  if (verifyCode.length === 4) {
-    if (verifyCode !== confirmVerifyCode.toString()) {
-      initInput(codeInput)
-      codeInput[0].focus()
-      alert('인증코드가 일치하지 않습니다.')
-      return
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
     }
+}
 
-    setCookie('codeToken', true)
-    goLoading()
-  }
+/**
+ * input 초기화
+ * @param inputs
+ */
+const initInput = (inputs) => {
+    inputs.forEach((input) => {
+        input.value = ''
+    })
+}
+
+/**
+ * 로딩 인터벌 시작
+ * 100이 되면 메인으로 이동
+ * @param loadingPercentNum 로딩 퍼센트 값
+ * @param loadingPercent 로딩 퍼센트 문자열
+ * @param loadingValue DOM Loading Value
+ * @param loadingBar DOM Loading Bar
+ */
+const startLoadingInterval = (loadingPercentNum, loadingPercent, loadingValue, loadingBar) => {
+    const loadingInterval = setInterval(() => {
+        loadingPercentNum += 1
+        loadingPercent = `${ loadingPercentNum }%`
+        loadingValue.textContent = loadingPercent
+        loadingBar.setAttribute('value', `${ loadingPercentNum }`)
+        if (loadingPercentNum === 100) {
+            clearInterval(loadingInterval)
+            location.href = '../main/main.html'
+        }
+    }, 100)
+}
+
+/**
+ * codeToken이 없을 때
+ * code 입력창으로 변경
+ */
+const codeTokenFalse = () => {
+    const inputWrapper = document.querySelector('.input-wrapper')
+    const InputForm = document.querySelector('.login-input-wrapper')
+    const inputTitle = document.querySelector('.input-title')
+    const inputSubTitle = document.querySelector('.input-title__sub')
+    
+    inputTitle.textContent = 'Please enter the code.'
+    inputSubTitle.textContent = 'Please contact the shipping company for confirmation code to register your new machine and enter the code you received.'
+    
+    // while (InputForm.firstChild) {
+    //   InputForm.removeChild(InputForm.firstChild)
+    // }
+    removeChild(InputForm)
+    changeToInputCode(InputForm)
+    setLoginAtt.setContact(inputWrapper)
+}
+
+/**
+ * code 입력창으로 변경
+ * @param InputForm
+ */
+const changeToInputCode = (InputForm) => {
+    for (let i = 0; i < 4; i++) {
+        const codeInput = document.createElement('input')
+        codeInput.setAttribute('class', `code-input code-input-${ i }`)
+        codeInput.setAttribute('type', 'text')
+        codeInput.setAttribute('maxlength', '1')
+        InputForm.appendChild(codeInput)
+        // 4개의 input에 이벤트 리스너 추가
+        codeInput.addEventListener('keyup', onInputCode)
+    }
+}
+
+/**
+ * code 입력 이벤트
+ * @param event
+ */
+const onInputCode = (event) => {
+    const codeInput = document.querySelectorAll('.code-input')
+    let verifyCode = ''
+    
+    // input max 1자리 => 1자리 다차면 다음 input으로 포커스 이동
+    if (event.target.value.length === 1 && event.target.nextElementSibling) {
+        event.target.nextElementSibling.focus()
+    }
+    
+    // 백스페이스 이전 input으로 포커스 이동
+    if (event.keyCode === 8 && event.target.previousElementSibling) {
+        event.target.previousElementSibling.focus()
+    }
+    
+    verifyCode = codeInput[0].value + codeInput[1].value + codeInput[2].value + codeInput[3].value
+    
+    // 코드가 4자리 다 차고 code가 일치하면 codeToken 생성 후 Loading
+    if (verifyCode.length === 4) {
+        if (verifyCode !== confirmVerifyCode.toString()) {
+            initInput(codeInput)
+            codeInput[0].focus()
+            alert('인증코드가 일치하지 않습니다.')
+            return
+        }
+        
+        setCookie('codeToken', true)
+        goLoading()
+    }
 }
